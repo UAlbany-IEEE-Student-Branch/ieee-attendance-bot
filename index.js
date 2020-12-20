f28231bac51c2e85fe6e4927c292fc9b59ad2ef1
acomanzo <ansonih17@gmail.com>
2020-12-20 17:48:05 -0500

    Switched from token in config to token in heroku, heroku couldn't find the config file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-const { token } = require('./config.json');
 
 const client = new Discord.Client();
 
@@ -113,4 +112,4 @@ client.on('message', message => {
     }
 });
 
-client.login(token);
+client.login(process.env.token);
