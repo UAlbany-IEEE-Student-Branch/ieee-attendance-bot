7df807eb8502474736d33edb1c5786523fb0d04f
acomanzo <ansonih17@gmail.com>
2021-02-21 22:30:41 -0500

    trying to get attendance off

---
+++ date-test.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0');
+const yyyy = today.getFullYear();
+const eventDate = mm + '/' + dd + '/' + yyyy;
+console.log(eventDate);
