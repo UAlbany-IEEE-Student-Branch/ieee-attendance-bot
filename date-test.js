5db1f807037129f0212bc0cc03956518da59e49b
acomanzo <ansonih17@gmail.com>
2021-02-21 22:32:35 -0500

    removed date test

--- date-test.js
+++
@@ -1,6 +0,0 @@
-const today = new Date();
-const dd = String(today.getDate()).padStart(2, '0');
-const mm = String(today.getMonth() + 1).padStart(2, '0');
-const yyyy = today.getFullYear();
-const eventDate = mm + '/' + dd + '/' + yyyy;
-console.log(eventDate);
